9b6866bb7af638104d0d1adf36b44cde4d8271c6
Sozuww <sozuww@gmail.com>
2025-02-28 11:39:17 -0800

    Bubble and Selection sort completed

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -7,19 +7,56 @@
  */
 public class SortingAlgorithms {
     // Change this value to change testing size
-    public static final int TEST_SIZE = 10;
+    public static final int TEST_SIZE = 500;
 
     // Change this value to change the range of numbers generated
     public static final int MAX_NUMBER_SIZE = 100;
 
     // Bubble Sort
     public static void bubbleSort(int[] arr) {
-        // TODO: Implement bubble sort
+        long startTime = System.currentTimeMillis();
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println(System.currentTimeMillis() - startTime + " ms");
     }
 
     // Selection Sort
     public static void selectionSort(int[] arr) {
-        // TODO: Implement selection sort
+        long startTime = System.currentTimeMillis();
+        int n = arr.length;
+
+        for (int i= 0; i < n - 1; i++)
+        {
+            int min = i;
+            for(int j = i + 1; j < n; j++)
+            {
+                if (arr[j] < arr[min])
+                {
+                    min = j;
+                }
+            }
+
+            if (min != i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+
+        System.out.println(System.currentTimeMillis() - startTime + " ms");
     }
 
     // Merge Sort
